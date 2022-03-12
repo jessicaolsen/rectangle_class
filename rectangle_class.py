546f6b877ca04ee89e6d9c72576dd6ff2c89454c
Jessica Olsen <jaolsen@yahoo.com>
2022-03-11 23:00:40 -0500

    Final code- passed test

--- rectangle_class.py
+++ rectangle_class.py
@@ -5,6 +5,7 @@ class Rectangle:
         self.width = width
         self.height = height
         
+        
     #Set width method
     def set_width(self, width) : 
         self.width = width 
@@ -24,7 +25,17 @@ class Rectangle:
     #Defining the Get Diagonal Method
     def get_diagonal(self) : 
         return (( self.width ** 2 ) + ( self.height ** 2 )) ** 0.5
-    
+   
+   #Defining if string is called
+    def __str__(self) : 
+        return "Rectangle(width={}, height={})".format(self.width,self.height)
+
+    #Determines how many times a shape will fit inside the given shape
+    def get_amount_inside(self, shape) : 
+        self.shape = shape
+        num_times = int(self.get_area() / (shape.width * shape.height))
+        return num_times
+
     #Set get picture method
     def get_picture(self) : 
         if self.height >= 50 : 
@@ -38,25 +49,40 @@ class Rectangle:
 
 class Square(Rectangle):
     
+    #Initializing Square class
     def __init__(self, side) : 
-        Rectangle.width = side
-        Rectangle.height = side
+        self.width = side
+        self.height = side
+        
+    #Defining Side = Width = Height
+    def set_side(self, side) : 
+        self.width = side
+        self.height = side
+    
+    #Set width method
+    def set_width(self, side) : 
+        self.width = side
+        self.height = side
 
-    def set_side(self) : 
-        pass
-
+    #Set height method
+    def set_height(self, side): 
+        self.height = side
+        self.width = side
+    
+    #Defining if String is called
+    def __str__(self) : 
+        return "Square(side={})".format(self.width)
 
 
 rect = Rectangle(5, 10)
 print(rect.get_area())
 rect.set_width(3)
 print(rect.get_perimeter())
-#print(rect)
 print(rect.get_picture())
 print(rect)
 
 sq = Square(9)
 print(sq.get_area())
-#sq.set_side(4)
-#print(sq.get_diagonal())
-#print(sq)
+sq.set_side(4)
+print(sq.get_diagonal())
+print(sq)
